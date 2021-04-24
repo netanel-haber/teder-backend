06dc5ef32dd3c7db7e84dcd0a8d76b549fd6055f
Netanel <netanel@cardlatch.com>
2021-04-24 11:31:47 +0300

    basic rooms

--- teder_backend/main.py
+++ teder_backend/main.py
@@ -1,8 +1,22 @@
 from flask import Flask, request
+from flask_socketio import SocketIO, join_room, send
+from .id_gen import gen_id
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@app.route("/")
-def index():
-    return request.args
+@socketio.on("message")
+def handle_message(data):
+    print("received message: " + data)
+
+@socketio.on("join")
+def join(room=None):
+    if room is None:
+      room = gen_id()
+    join_room(int(room))
+    return room
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0")
